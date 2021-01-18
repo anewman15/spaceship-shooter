851d5c31e2b24eeed53e71a3bb163450174440c1
Abdullah Numan <anewman15@hotmail.com>
2021-01-18 17:55:43 +0600

    Create Entity and PlayerShip classes for adding to PlayScene

---
+++ src/js/entities/PlayerShip.js
@@ -0,0 +1,59 @@
+import 'phaser';
+import Entity from './Entity';
+
+export default class PlayerShip extends Entity {
+  constructor(scene, x, y, key) {
+    super(scene, x, y, key, 'PlayerShip');
+    this.setData("speed", 200);
+    this.play("sprPlayer");
+    this.setData("isShooting", false);
+    this.setData("timerShootDelay", 10);
+    this.setData("timerShootTick", this.getData("timerShootDelay") - 1);
+  }
+
+  moveUp() {
+    this.body.velocity.y = -this.getData("speed");
+  }
+
+  moveDown() {
+    this.body.velocity.y = this.getData("speed");
+  }
+
+  moveLeft() {
+    this.body.velocity.x = -this.getData("speed");
+  }
+
+  moveRight() {
+    this.body.velocity.x = this.getData("speed");
+  }
+
+  update() {
+    this.body.setVelocity(0, 0);
+    this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
+    this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+    if (this.getData("isShooting")) {
+      if (this.getData("timerShootTick") < this.getData("timerShootDelay")) {
+        this.setData("timerShootTick", this.getData("timerShootTick") + 1); // every game update, increase timerShootTick by one until we reach the value of timerShootDelay
+      }
+      else { // when the "manual timer" is triggered:
+        var laser = new PlayerLaser(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+
+        this.scene.sfx.laser.play(); // play the laser sound effect
+        this.setData("timerShootTick", 0);
+      }
+    }
+  }
+
+  onDestroy() {
+    this.scene.time.addEvent({ // go to game over scene
+      delay: 1000,
+      callback: function() {
+        this.scene.scene.start("SceneGameOver");
+      },
+      callbackScope: this,
+      loop: false
+    });
+  }
+}
