d748bd5ba7aebf881fb14f8f24c574fcc7e34c94
Abdullah Numan <anewman15@hotmail.com>
2021-01-18 17:59:11 +0600

    Add config options to config.js

--- src/js/config.js
+++ src/js/config.js
@@ -4,5 +4,14 @@ export default {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 800,
-  height: 600
+  height: 600,
+  backgroundColor: "black",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  pixelArt: true,
+  roundPixels: true
 };
