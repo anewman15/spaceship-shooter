5ce070c63a5041a994cceaac2f6c7b988cc336f7
Abdullah Numan <anewman15@hotmail.com>
2021-01-20 15:53:56 +0600

    Add GameOverScene

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import BootScene from './scenes/BootScene';
 import PreloaderScene from './scenes/PreloaderScene';
 import TitleScene from './scenes/TitleScene';
 import CreditsScene from './scenes/CreditsScene';
+import LeaderboardScene from './scenes/LeaderboardScene';
+import GameOverScene from './scenes/GameOverScene';
 import PlayScene from './scenes/PlayScene';
 
 class Game extends Phaser.Game {
@@ -13,7 +15,9 @@ class Game extends Phaser.Game {
 		this.scene.add('Preloader', PreloaderScene);
 		this.scene.add('Title', TitleScene);
 		this.scene.add('Credits', CreditsScene);
+		this.scene.add('Leaderboard', LeaderboardScene);
 		this.scene.add('Play', PlayScene);
+		this.scene.add('GameOver', GameOverScene);
 		this.scene.start('Boot');
 	}
 }
---
+++ src/js/scenes/GameOverScene.js
@@ -0,0 +1,20 @@
+import 'phaser';
+import config from '../config';
+import Button from '../entities/Button';
+
+export default class GameOverScene extends Phaser.Scene {
+  constructor() {
+    super('GameOver');
+  }
+
+  create() {
+    this.gameOverText = this.add.text(config.width/2, config.height/2 - 200, 'Game Over', { fontSize: '48px', fill: '#fff' });
+    this.gameOverText.setOrigin(0.5, 0.5);
+
+    this.scoreText = this.add.text(config.width/2, config.height/2 - 100, 'Your Score: 0', { fontSize: '32px', fill: '#fff' });
+    this.scoreText.setOrigin(0.5, 0.5);
+
+    this.playAgainButton = new Button(this, config.width/2, config.height/2 + 50, 'blueButton', 'greenButton', 'Play Again', 'Play');
+    this.gameButton = new Button(this, config.width/2, config.height/2 + 120, 'blueButton', 'greenButton', 'Leaderboard', 'Leaderboard');
+  }
+};
