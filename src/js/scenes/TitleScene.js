ffea0ae19984860ec8f392487b008c91aa7a01a0
Abdullah Numan <anewman15@hotmail.com>
2021-01-21 18:16:38 +0600

    Add title to TitleScene

--- src/js/scenes/TitleScene.js
+++ src/js/scenes/TitleScene.js
@@ -8,8 +8,10 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
-    this.gameButton = new Button(this, config.width/2, config.height/2 - 80, 'blueButton', 'greenButton', 'Play', 'Play');
-    this.gameButton = new Button(this, config.width/2, config.height/2, 'blueButton', 'greenButton', 'Leaderboard', 'Leaderboard');
-    this.creditsButton = new Button(this, config.width/2, config.height/2 + 80, 'blueButton', 'greenButton', 'Credits', 'Credits');
+    this.titleText = this.add.text(config.width/2, config.height/2 - 200, 'SpaceShip Shooter', { fontSize: '64px', fill: '#fff' });
+    this.titleText.setOrigin(0.5, 0.5);
+    this.gameButton = new Button(this, config.width/2, config.height/2 - 20, 'blueButton', 'greenButton', 'Play', 'Play');
+    this.gameButton = new Button(this, config.width/2, config.height/2 + 60, 'blueButton', 'greenButton', 'Leaderboard', 'Leaderboard');
+    this.creditsButton = new Button(this, config.width/2, config.height/2 + 140, 'blueButton', 'greenButton', 'Credits', 'Credits');
   }
 };
