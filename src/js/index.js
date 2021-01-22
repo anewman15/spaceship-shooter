e46bc033e1f6ea0402c4cb62eac5e12e028a396f
Abdullah Numan <anewman15@hotmail.com>
2021-01-22 20:37:20 +0600

    Remove game from index and start game with event listener

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,3 @@
-import 'phaser';
-import config from './config';
-import User from  './user-data/User';
-import BootScene from './scenes/BootScene';
-import PreloaderScene from './scenes/PreloaderScene';
-import TitleScene from './scenes/TitleScene';
-import CreditsScene from './scenes/CreditsScene';
-import LeaderboardScene from './scenes/LeaderboardScene';
-import GameOverScene from './scenes/GameOverScene';
-import PlayScene from './scenes/PlayScene';
-import formEventListener from './user-data/form';
+import { formEventListener } from './user-data/form';
 
-class Game extends Phaser.Game {
-	constructor() {
-		super(config);
-		const user = new User();
-		this.globals = { user };
-		this.scene.add('Boot', BootScene);
-		this.scene.add('Preloader', PreloaderScene);
-		this.scene.add('Title', TitleScene);
-		this.scene.add('Credits', CreditsScene);
-		this.scene.add('Leaderboard', LeaderboardScene);
-		this.scene.add('Play', PlayScene);
-		this.scene.add('GameOver', GameOverScene);
-		this.scene.start('Boot');
-	}
-}
-
-window.game = new Game();
+formEventListener();
