14607e32b2a6cc79484f30a0b62d77134a4ad3f6
Abdullah Numan <anewman15@hotmail.com>
2021-01-22 18:32:55 +0600

    Create getTopScores api call and add top scores to LeaderboardScene

--- src/js/scenes/LeaderboardScene.js
+++ src/js/scenes/LeaderboardScene.js
@@ -1,6 +1,8 @@
 import 'phaser';
 import config from '../config';
 import Button from '../entities/Button';
+import { getTopScores } from '../user-data/api';
+
 
 export default class GameOverScene extends Phaser.Scene {
   constructor() {
@@ -8,14 +10,38 @@ export default class GameOverScene extends Phaser.Scene {
   }
 
   create() {
-    this.topScoresText = this.add.text(config.width/2, config.height/2 - 250, 'Top Scores', { fontSize: '32px', fill: '#fff' });
-    this.topScoresText.setOrigin(0.5, 0.5);
+    this.topScoresText = this.add.text(
+      config.width/2,
+      config.height/2 - 250,
+      'Top Scores',
+      { fontSize: '32px', fill: '#fff' }
+    ).setOrigin(0.5, 0.5);
+
+    const scoresStyle = {
+      fontSize: '16px',
+      fill: '#fff'
+    }
 
     this.user = this.sys.game.globals.user;
-    this.scoreText = this.add.text(config.width/2, config.height/2 - 200, `Your Score: ${this.user.score}`, { fontSize: '16px', fill: '#fff' });
-    this.scoreText.setOrigin(0.5, 0.5);
+    this.scoreText = this.add.text(
+      config.width/2,
+      config.height/2 - 210,
+      `Your Score: ${this.user.score}`,
+      scoresStyle
+    ).setOrigin(0.5, 0.5);
 
-    this.playAgainButton = new Button(this, config.width/2, config.height/2 + 180, 'blueButton', 'greenButton', 'Play Again', 'Play');
+    getTopScores().then(users => {
+      users.forEach((user, index) => {
+        console.log(user);
+        let height = config.height/2 - 180 + index*40;
+        this.add.text(
+          config.width/3,
+          height,
+          `${index + 1}. ${user.user} -- ${user.score}`, scoresStyle
+        ).setOrigin(0, 0.5);
+      });
+    });
+
     this.menuButton = new Button(this, config.width/2, config.height/2 + 240, 'blueButton', 'greenButton', 'Menu', 'Title');
   }
 };
--- src/js/user-data/api.js
+++ src/js/user-data/api.js
@@ -15,3 +15,17 @@ export const postScore = async (apiUri, userInfo) => {
     return;
   }
 }
+
+export const getTopScores = async () => {
+  const uri = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HlhJM0tltblLcmbw1VvF/scores/';
+  try {
+    const response = await fetch(uri);
+    const data = await response.json();
+    const result = data.result;
+    result.sort((a, b) => b.score - a.score);
+    const topScores = data.result.slice(0, 10);
+    return topScores;
+  } catch(error) {
+    return "Couldn't retrieve Leaderboard information :("
+  }
+}
